21eb0409048146a2be6f5cd02684c163cac9780d
S <sumanth.s@hpe.com>
2020-12-10 15:53:54 +0530

    Checkpoint

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import logging
 from datetime import datetime
 import sys
-
+import re
 
 def sshCommand(hostname, port, username, password, command):
     sshClient = paramiko.SSHClient()                                   # create SSHClient instance
@@ -15,72 +15,86 @@ def sshCommand(hostname, port, username, password, command):
     #print(sshClient.load_system_host_keys())  
     sshClient.connect(hostname, port, username, password)
     stdin, stdout, stderr = sshClient.exec_command(command)
-    result = stdout.readlines()
-    #result = ''.join(stdout.readlines())
+    result = ''.join(stdout.readlines())
     #print(result)
-    return  result[-1].strip()
+    
+    return result
+    
 
 
     
-    for elem in data:
-        flag=False
-        try:
-            if elem[3]=='Linux' and elem[0][0]!='#':
-                #print("enter")
-                flag=True
-                value = sshCommand(elem[0],22,elem[1],elem[2],'ofed_uninstall.sh --force')
-                print(value)
-                compare = value.rstrip("\n")
-                #print("Asd")
-                if compare=='Uninstall finished successfully':
-                    #print("asad")
-                    return elem[0]+' : ofed uninstallation was successful'
-                else:
-                    #print("asad")
-                    return  elem[0]+' : ofed uninstallation was unsuccessful'
-        except:
-            if flag==True and elem[0][0]!='#':
-                return elem[0]+' : ofed already been uninstalled'
-                #print("H")
-    
+  
     
     
 dictionary = dict()
 file = open('reference.txt')
 for each in file:
-    if each != '\n':
+    if each != '\n' and each[0]!='#':
         dictionary[each.split()[0]] = each.split()[1]
-#print(dictionary)
+print(dictionary)
 
 
 iso_list = os.listdir(os.getcwd())
 #print(iso_list)
-
-for elem in dictionary.keys()::
+def uninstall_ofed():
+    string = ''
+    for elem in dictionary.keys():
         flag=False
         try:
-            if dictionary[elem][0]!='#':
-                #print("enter")
-                flag=True
-                value = sshCommand(dictionary[elem],22,'root','Hptc_ib','ofed_uninstall.sh --force')
-                print(value)
-                compare = value.rstrip("\n")
-                #print("Asd")
-                if compare=='Uninstall finished successfully':
-                    #print("asad")
-                    return dictionary[elem]+' : ofed uninstallation was successful'
-                else:
-                    #print("asad")
-                    return  dictionary[elem]+' : ofed uninstallation was unsuccessful'
+            flag=True
+            value = sshCommand(dictionary[elem],22,'root','Hptc_ib','ofed_uninstall.sh --force')
+            #print(value)
+            compare = value.strip('\n').split('\n')[-1]
+            print(compare)
+            if compare=='Uninstall finished successfully':
+                string += dictionary[elem]+' : ofed uninstallation was successful\n'
+            elif 'command not found' in compare:
+                string += dictionary[elem]+' : ofed already been uninstalled\n'
+            elif compare == '':
+                string += dictionary[elem]+' : ofed already been uninstalled\n'
+            else:
+                string += dictionary[elem]+' : ofed uninstallation was unsuccessful\n'
         except:
-            if flag==True and dictionary[elem][0]!='#':
-                return dictionary[elem]+' : ofed already been uninstalled'
-'''for item in iso_list:
+            if flag==True and elem[0]!='#':
+                string += dictionary[elem]+' : ofed already been uninstalled\n'
+    return string
+
+print(uninstall_ofed())
+
+def iso_copy():
+    string = ''
     for each in dictionary.keys():
+        for item in iso_list:
         
-        if each in item and 'alternate-aarch64' not in item:
-            print(item+" was copied to "+dictionary[each])
-            os.system('cmd /c "pscp -r -pw Hptc_ib "'+item+'" root@"'+dictionary[each]+'":/root/"')
-        if 'MLNX_OFED Documentation Rev 5.1-2.5.8.0__12_02_2020' in item:
-            os.system('cmd /c "pscp -r -pw Hptc_ib "'+item+'" root@"'+dictionary[each]+'":/root/"')'''
+            if each in item and 'alternate-aarch64' not in item:
+                command = 'mkdir '+item.strip('.iso')
+                sshCommand(dictionary[each],22,'root','Hptc_ib',command)
+                os.system('cmd /c "pscp -r -pw Hptc_ib "'+item+'" root@"'+dictionary[each]+'":/root/"'+item.strip('.iso')+'"/" > nul')
+                string += item+" was copied to "+dictionary[each]+'\n'
+            
+                os.system('cmd /c "pscp -r -pw Hptc_ib \"NVIDIA® MLNX_OFED Documentation Rev 5.1-2.5.8.0__12_02_2020.pdf\" root@"'+dictionary[each]+'":/root/"'+item.strip('.iso')+'"/" > nul')
+                string += "NVIDIA® MLNX_OFED Documentation Rev 5.1-2.5.8.0__12_02_2020 was copied to "+dictionary[each]+'\n'
+
+    return string
+
+print(iso_copy())
     
+
+def iso_mount():
+    for each in dictionary.keys():
+        for item in iso_list:
+            if each in item:
+                sshCommand(dictionary[each],22,'root','Hptc_ib','umount /mnt')
+                sshCommand(dictionary[each],22,'root','Hptc_ib','mount /root/'+item.strip('.iso')+'/'+item+' /mnt')
+                sshCommand(dictionary[each],22,'root','Hptc_ib','ls /mnt')
+                store_data = sshCommand(dictionary[each],22,'root','Hptc_ib','cd /mnt; ./mlnxofedinstall --force')
+                ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+                write_data = ansi_escape.sub('', store_data)
+                print(write_data)
+                with open(item.strip('.iso')+'.txt','w') as f:
+                    f.write(write_data)
+                sshCommand(dictionary[each],22,'root','Hptc_ib','cd /root; tar -czvf '+item.strip('.iso')+'.tar.gz '+item.strip('.iso'))
+                os.system('cmd /c "pscp -r -pw Hptc_ib root@"'+dictionary[each]+'":/root/"'+item.strip('.iso')+'".tar.gz ." > nul')
+               
+
+iso_mount()
